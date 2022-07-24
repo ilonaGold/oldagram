bcbdb43b0f7a7d8e3920800548249f647e4f398d
Ilona Goldmane <songa17@inbox.lv>
2022-07-24 11:50:00 +0300

    Create add likes function

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        didLike: false
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +16,8 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        didLike: false
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +26,8 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        didLike: false
     }
 ]
 
@@ -67,4 +70,23 @@ function renderPosts() {
 
 renderPosts();
 
+const likes = document.getElementsByClassName("icon-heart");
+let likesCount = document.querySelectorAll(".likes-count");
 
+let addLikes = (i) => {
+    if (posts[i].didLike === false) {
+        posts[i] = {
+            ...posts[i],
+            likes: posts[i].likes + 1,
+            didLike: true
+        }
+    } else {
+        posts[i] = {
+            ...posts[i],
+            likes: posts[i].likes - 1,
+            didLike: false
+        }
+    }
+    rest();
+    renderPosts();
+}
